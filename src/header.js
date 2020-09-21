a11738e911c39fa0f8f34130b20bad400c449148
hillarioh <okerioh@gmail.com>
2020-09-21 14:04:46 +0300

    add header image transition.

---
+++ src/header.js
@@ -0,0 +1,15 @@
+import {gsap} from 'gsap';
+
+
+
+
+
+let images = ['../assets/images/one.jpg','../assets/images/one.jpg','../assets/images/one.jpg'];
+
+
+const image = document.createElement('img');
+
+image.src = images[0];
+
+
+export { image}
---
+++ src/index.js
@@ -0,0 +1,36 @@
+const headTitle = document.getElementById("head-title");
+const headContent = document.getElementById("head-body");
+const headLink = document.getElementById("head-link");
+
+let headlines = [
+  {
+    title: "Meet the boss",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta ratione saepe quae repellendus eius voluptas.",
+    link: "Learn more",
+  },
+  {
+    title: "Covid-19",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta ratione saepe quae repellendus eius voluptas.",
+    link: "Response",
+  },
+  {
+    title: "Stories of change",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta ratione saepe quae repellendus eius voluptas.",
+    link: "Read stories",
+  },
+];
+
+var counter = 1;
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 3) {
+    counter = 1;
+  }
+  headTitle.textContent = headlines[counter - 1].title;
+  headContent.textContent = headlines[counter - 1].content;
+  headLink.textContent = headlines[counter - 1].link;
+}, 5000);
